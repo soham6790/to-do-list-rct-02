878851b68ce844267c480247d885ae20cb39ce9c
soham <sohamdbu@bu.edu>
2025-03-15 23:42:24 -0400

    Adding TodoContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { TodoProvider } from './contexts';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [todos,settodos] = useState([]);
+
+  const addTodo = (todo) => {
+    settodos((prevTodos) => [{id:Date.now(), ...todo}, ...prevTodos]);
+  }
+
+  const updateTodo = (id, todo) =>{
+    settodos((prevTodos) => prevTodos.map((ptodo) => (ptodo.id===id ? todo : ptodo)));
+  }
+
+  const deleteTodo = (id) =>{
+    settodos((prevTodos) => prevTodos.filter((ptodo) => (ptodo.id!==id)));
+  }
+
+  const toggleComplete = (id) =>{
+    settodos((prevTodos) => prevTodos.map((ptodo) => (ptodo.id===id ?  {...ptodo,completed: !ptodo.completed}: ptodo)));
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </TodoProvider>
   )
 }
 
